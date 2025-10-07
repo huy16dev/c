8363964e76cbd4f7ed7a6f1cace74b11ddfd3163
huy16dev <phandragonpro123@gmail.com>
2025-10-07 09:42:28 +0700

    Create 06.c

---
+++ phanvanphonghuy_B25DTCN178_session02/06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = 2;
+    int d = 1;
+
+    int A = a * b - 2 * c + 3 * (a - d);
+
+    printf("A = %d\n", A);
+
+    return 0;
+}
