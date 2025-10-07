ae8579dfb6355f8111a71e7a5e73c54c470804ac
huy16dev <phandragonpro123@gmail.com>
2025-10-07 09:41:06 +0700

    Create 03.c

---
+++ phanvanphonghuy_B25DTCN178_session02/03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a = 8;
+    int b = 4;
+
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    int thuong = a / b;
+
+    printf("tong = %d\n", tong);
+    printf("hieu = %d\n", hieu);
+    printf("tich = %d\n", tich);
+    printf("thuong = %d\n", thuong);
+
+    return 0;
+}
