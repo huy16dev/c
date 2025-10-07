d9345fdea222177695e7d74dfe7f2ba3b2eb59a6
huy16dev <phandragonpro123@gmail.com>
2025-10-07 09:59:32 +0700

    Create 02.c

---
+++ phanvanphonghuy_B25DTCN178_session04/02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("nhap mot so nguyen: ");
+    scanf("%d", &n);
+    if (n % 2 == 0)
+        printf("so vua nhap la so chan\n");
+    else
+        printf("so vua nhap la so le\n");
+    return 0;
+}
