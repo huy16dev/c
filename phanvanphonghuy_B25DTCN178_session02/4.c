ba7441a11c2589466ab1aa8c321aea609520bcca
huy16dev <phandragonpro123@gmail.com>
2025-10-07 09:41:34 +0700

    Create 4.c

---
+++ phanvanphonghuy_B25DTCN178_session02/4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int dai = 10;
+    int rong = 5;
+
+    int chuvi = (dai + rong) * 2;
+    int dientich = dai * rong;
+
+    printf("chu vi = %d\n", chuvi);
+    printf("dien tich = %d\n", dientich);
+
+    return 0;
+}
