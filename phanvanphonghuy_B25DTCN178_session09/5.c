67abe3be95beb328a29035756b99e9f8febfe714
huy16dev <phandragonpro123@gmail.com>
2025-10-29 04:48:13 +0700

    Create 5.c

---
+++ phanvanphonghuy_B25DTCN178_session09/5.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("nhap so n (so o: n*n): ");
+	scanf("%d", &n);
+	int arr[n][n];
+	//gan value
+	for(int i = 0; i<n; i++) {
+		for(int j = 0; j<n; j++) {
+			printf("gan gia tri cho arr[%d][%d] :", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	//in bang ma tran nxn
+	printf("bang gia tri vua nhap: \n");
+	for(int i = 0; i<n; i++) {
+		for(int j = 0; j<n; j++) {
+			printf("%d  ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	//tinh tong gia tri phan tu duong bien
+	int sum = 0;
+	for(int i = 0; i<n; i++) {
+		for(int j = 0; j<n; j++) {
+			//filter duong bien
+			if(i == 0 || j ==0 || i == (n-1) || j == (n-1)) {
+				sum += arr[i][j];
+			}
+		}
+	}
+
+	printf("tong cac gia tri duong bien la: %d", sum);
+
+	  /*in:
+    nhap so n (so o: n*n): 3
+        gan gia tri cho arr[0][0] :1
+        gan gia tri cho arr[0][1] :1
+        gan gia tri cho arr[0][2] :1
+        gan gia tri cho arr[1][0] :1
+        gan gia tri cho arr[1][1] :100
+        gan gia tri cho arr[1][2] :1
+        gan gia tri cho arr[2][0] :1
+        gan gia tri cho arr[2][1] :1
+        gan gia tri cho arr[2][2] :1
+        out:
+        bang gia tri vua nhap: 
+        1  1  1  
+        1  100  1  
+        1  1  1  
+        tong cac gia tri duong bien la: 8*/
+
+	/*
+	//vong lap tim max
+	int i = 0, j =0,x, y;
+	int max = arr[0][0];
+	for(; i<n; i++){
+	    for(; j<n; j++){
+	        if(arr[i][j] > max){
+	            max = arr[i][j];
+	            x = i;
+	            y =j;
+	        }
+	    }
+	}
+	printf("so lon nhat trong bang la: arr[%d][%d] = %d", x, y, max);*/
+	return 0;
+
+}
